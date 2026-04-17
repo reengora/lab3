fb50b199566ee422c931463660dc3f5fdba293a2
Your Name <you@example.com>
2026-04-17 19:01:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -69,7 +69,13 @@ void insertMap(HashMap * map, char * key, void * value) {
 //   c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
 // Recuerde actualizar el índice current a la posición encontrada. Recuerde que el arreglo es circular.
 
-Pair * searchMap(HashMap * map,  char * key) {   
+Pair * searchMap(HashMap * map,  char * key) {
+    int pos = key % map->size;
+    if(is_equal(map->buckets[pos]->key,key) == 1)return map->buckets[pos]->value;
+    while(map->buckets[pos]->key != NULL){
+        pos = (pos +1)% map->capacity;
+        if(is_equal(map->buckets[pos]->key,key) == 1)return map->buckets[pos]->value;
+    }
 
 
     return NULL;
