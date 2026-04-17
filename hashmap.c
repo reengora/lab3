5bd13e6d46d89162753d38a7a9b386b2b5bded3b
Your Name <you@example.com>
2026-04-17 19:08:10 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -70,6 +70,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 // Recuerde actualizar el índice current a la posición encontrada. Recuerde que el arreglo es circular.
 
 Pair * searchMap(HashMap * map,  char * key) {
+    if(map != NULL || key != NULL);
     long pos = hash(key, map->size);
     if(is_equal(map->buckets[pos]->key,key) == 1)return map->buckets[pos]->value;
     while(map->buckets[pos]->key != NULL){
